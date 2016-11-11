b989a265e63a603bf91f4dca41e679caefda5746
akoper <andrew.koper@gmail.com>
2016-11-11 15:18:31 -0500

    migration file to push user db modifications to production

---
+++ database/migrations/2016_11_11_194823_make_some_cols_nullable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeSomeColsNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('hashtag')->nullable()->change();
+            $table->string('oneFName')->nullable()->change();
+            $table->string('oneLName')->nullable()->change();
+            $table->string('twoLName')->nullable()->change();
+            $table->string('twoFName')->nullable()->change();
+            $table->string('date')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
